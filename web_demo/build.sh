0a7c06bf0c1d5786abae4016fd2517ac7b406d81
Jonathan Zhao <jinqiang.zhao@utoronto.ca>
2025-06-16 11:45:37 -0400

    Build for Docker as well.

--- web_demo/build.sh
+++ web_demo/build.sh
@@ -5,3 +5,9 @@ cd $selfDir
 
 GOOS=linux GOARCH=amd64 go build .
 scp web_demo w0:~
+
+# container image build
+docker build -t utsso/canheit2025_demo .
+if [[ X"$1" == X"prod" ]]; then
+    docker push utsso/canheit2025_demo
+fi
